5f5a7caf83b74592c697537288da6a7cd21fe981
王帅杰 <shuaijie.wang@ziel.cn>
2026-03-30 11:46:34 +0800

    fix: 修正端口映射和应用启动端口为19014

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=11180, reload=True,access_log=False)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=19014, reload=True,access_log=False)
